f0af8d22ad3d505f3a0dc96f63cb1c1c5e18b071
Mayerlin Rojas Perez <rojasmayer@gmail.com>
2023-02-22 18:57:56 -0500

    solo modifique un poco el acerca de

--- ControlEntrada/ControlEntrada/ModuloAcercaDe.Designer.cs
+++ ControlEntrada/ControlEntrada/ModuloAcercaDe.Designer.cs
@@ -103,7 +103,7 @@
             this.labelMenu2.AutoSize = true;
             this.labelMenu2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelMenu2.ForeColor = System.Drawing.Color.Black;
-            this.labelMenu2.Location = new System.Drawing.Point(218, 47);
+            this.labelMenu2.Location = new System.Drawing.Point(178, 47);
             this.labelMenu2.Name = "labelMenu2";
             this.labelMenu2.Size = new System.Drawing.Size(415, 22);
             this.labelMenu2.TabIndex = 41;
@@ -114,7 +114,7 @@
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(466, 320);
+            this.label5.Location = new System.Drawing.Point(426, 320);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(97, 15);
             this.label5.TabIndex = 40;
@@ -125,7 +125,7 @@
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(70, 259);
+            this.label4.Location = new System.Drawing.Point(30, 259);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(255, 60);
             this.label4.TabIndex = 39;
@@ -138,7 +138,7 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(347, 124);
+            this.label3.Location = new System.Drawing.Point(307, 124);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(191, 30);
             this.label3.TabIndex = 38;
@@ -149,7 +149,7 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(402, 99);
+            this.label2.Location = new System.Drawing.Point(362, 99);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(79, 15);
             this.label2.TabIndex = 37;
@@ -160,7 +160,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(324, 79);
+            this.label1.Location = new System.Drawing.Point(284, 79);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(239, 20);
             this.label1.TabIndex = 36;
@@ -170,7 +170,7 @@
             // 
             this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(341, 238);
+            this.pictureBox2.Location = new System.Drawing.Point(301, 238);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(175, 123);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -181,7 +181,7 @@
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(82, 47);
+            this.pictureBox1.Location = new System.Drawing.Point(42, 47);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(228, 209);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
