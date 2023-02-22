c081f33883be0bef3595df355ca68c855f3fca19
Mayerlin Rojas Perez <rojasmayer@gmail.com>
2023-02-21 20:19:42 -0500

    la vista de menu esta lista

--- ControlEntrada/ControlEntrada/Menu.Designer.cs
+++ ControlEntrada/ControlEntrada/Menu.Designer.cs
@@ -28,12 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelMenu = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelMenu2 = new System.Windows.Forms.Label();
+            this.btnUsuariosSistema = new System.Windows.Forms.Button();
+            this.btnRegistroPersona = new System.Windows.Forms.Button();
+            this.btnRegEntradaSalida = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnAcercaDe = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(169)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.labelMenu);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(705, 39);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelMenu
+            // 
+            this.labelMenu.AutoSize = true;
+            this.labelMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMenu.ForeColor = System.Drawing.Color.White;
+            this.labelMenu.Location = new System.Drawing.Point(12, 9);
+            this.labelMenu.Name = "labelMenu";
+            this.labelMenu.Size = new System.Drawing.Size(254, 22);
+            this.labelMenu.TabIndex = 0;
+            this.labelMenu.Text = "Control de Entrada - Salida";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ControlEntrada.Properties.Resources.LogoSena;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(166, 172);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelMenu2
+            // 
+            this.labelMenu2.AutoSize = true;
+            this.labelMenu2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMenu2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(169)))), ((int)(((byte)(0)))));
+            this.labelMenu2.Location = new System.Drawing.Point(184, 110);
+            this.labelMenu2.Name = "labelMenu2";
+            this.labelMenu2.Size = new System.Drawing.Size(498, 26);
+            this.labelMenu2.TabIndex = 2;
+            this.labelMenu2.Text = "CENTRO DE BIOTECNOLOGIA INDUSTRIAL";
+            // 
+            // btnUsuariosSistema
+            // 
+            this.btnUsuariosSistema.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnUsuariosSistema.BackgroundImage")));
+            this.btnUsuariosSistema.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnUsuariosSistema.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUsuariosSistema.FlatAppearance.BorderSize = 0;
+            this.btnUsuariosSistema.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsuariosSistema.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUsuariosSistema.Location = new System.Drawing.Point(208, 193);
+            this.btnUsuariosSistema.Name = "btnUsuariosSistema";
+            this.btnUsuariosSistema.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnUsuariosSistema.Size = new System.Drawing.Size(111, 136);
+            this.btnUsuariosSistema.TabIndex = 3;
+            this.btnUsuariosSistema.Text = "Usuarios del Sistema";
+            this.btnUsuariosSistema.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnUsuariosSistema.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistroPersona
+            // 
+            this.btnRegistroPersona.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRegistroPersona.BackgroundImage")));
+            this.btnRegistroPersona.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRegistroPersona.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegistroPersona.FlatAppearance.BorderSize = 0;
+            this.btnRegistroPersona.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistroPersona.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistroPersona.Location = new System.Drawing.Point(355, 173);
+            this.btnRegistroPersona.Name = "btnRegistroPersona";
+            this.btnRegistroPersona.Size = new System.Drawing.Size(130, 156);
+            this.btnRegistroPersona.TabIndex = 4;
+            this.btnRegistroPersona.Text = "Registro de Personas";
+            this.btnRegistroPersona.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnRegistroPersona.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRegistroPersona.UseVisualStyleBackColor = true;
+            // 
+            // btnRegEntradaSalida
+            // 
+            this.btnRegEntradaSalida.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRegEntradaSalida.BackgroundImage")));
+            this.btnRegEntradaSalida.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRegEntradaSalida.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegEntradaSalida.FlatAppearance.BorderSize = 0;
+            this.btnRegEntradaSalida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegEntradaSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegEntradaSalida.Location = new System.Drawing.Point(522, 163);
+            this.btnRegEntradaSalida.Name = "btnRegEntradaSalida";
+            this.btnRegEntradaSalida.Size = new System.Drawing.Size(130, 166);
+            this.btnRegEntradaSalida.TabIndex = 5;
+            this.btnRegEntradaSalida.Text = "Registro de Entrada/Salida";
+            this.btnRegEntradaSalida.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnRegEntradaSalida.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRegEntradaSalida.UseVisualStyleBackColor = true;
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnReportes.BackgroundImage")));
+            this.btnReportes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnReportes.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReportes.FlatAppearance.BorderSize = 0;
+            this.btnReportes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReportes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReportes.Location = new System.Drawing.Point(264, 346);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(130, 132);
+            this.btnReportes.TabIndex = 6;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnReportes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnReportes.UseVisualStyleBackColor = true;
+            // 
+            // btnAcercaDe
+            // 
+            this.btnAcercaDe.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAcercaDe.BackgroundImage")));
+            this.btnAcercaDe.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnAcercaDe.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAcercaDe.FlatAppearance.BorderSize = 0;
+            this.btnAcercaDe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAcercaDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAcercaDe.Location = new System.Drawing.Point(449, 346);
+            this.btnAcercaDe.Name = "btnAcercaDe";
+            this.btnAcercaDe.Size = new System.Drawing.Size(130, 132);
+            this.btnAcercaDe.TabIndex = 7;
+            this.btnAcercaDe.Text = "Acerca de";
+            this.btnAcercaDe.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAcercaDe.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAcercaDe.UseVisualStyleBackColor = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Menu";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(705, 506);
+            this.Controls.Add(this.btnUsuariosSistema);
+            this.Controls.Add(this.btnReportes);
+            this.Controls.Add(this.btnRegEntradaSalida);
+            this.Controls.Add(this.btnRegistroPersona);
+            this.Controls.Add(this.labelMenu2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAcercaDe);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelMenu;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelMenu2;
+        private System.Windows.Forms.Button btnUsuariosSistema;
+        private System.Windows.Forms.Button btnRegistroPersona;
+        private System.Windows.Forms.Button btnRegEntradaSalida;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.Button btnAcercaDe;
     }
 }
